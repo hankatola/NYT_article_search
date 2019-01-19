6f39fc3860e39a5e224412cb1e767e69c710ea5f
johnale <johnale@cisco.com>
2019-01-19 12:29:50 -0500

    add css folder

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
 $.get('https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20150101&end_date=20160101&q=war&sort=newest&api-key=DUboS1XV1GSEARcYle8Upu5f9MORBuaq')
 .then((data) => {
-    console.log(data);
+    const articleData = data.response.docs;
+    articleData.forEach(article => {
+        console.log(article);
+    });
 })
 .catch((error) => console.log(`you have an error: ${error}`));
