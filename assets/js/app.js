7173c6256613f7b17f3ecfff15f055266ae89c0b
laurenkerns <laurenkerns@gmail.com>
2019-01-19 12:49:29 -0500

    update CSS path

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
-queryURL = 'https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20150101&end_date=20160101&q=war&sort=newest&api-key=DUboS1XV1GSEARcYle8Upu5f9MORBuaq'
-
-$.get(queryURL)
-.then((data) => {
-
-
-});
-=======
 $.get('https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20150101&end_date=20160101&q=war&sort=newest&api-key=DUboS1XV1GSEARcYle8Upu5f9MORBuaq')
 .then((data) => {
     const articleData = data.response.docs;
     articleData.forEach(article => {
         console.log(article.pub_date);
+        console.log(article.headline.main);
+        console.log(article.byline.person.firstname + " " + article.byline.person.lastname);
+        console.log(article.snippet);
+        console.log(article.web_url);
+
+
+
     });
 })
 .catch((error) => console.log(`you have an error: ${error}`));
->>>>>>> 08dbf5a2fe7bb8c61f284b0085f84c7466a1bbfa
