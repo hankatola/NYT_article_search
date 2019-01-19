d818c760af2bd0e600df75bd72d81922dfa06546
laurenkerns <laurenkerns@gmail.com>
2019-01-19 12:37:26 -0500

    inital commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+queryURL = 'https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20150101&end_date=20160101&q=war&sort=newest&api-key=DUboS1XV1GSEARcYle8Upu5f9MORBuaq'
+
+$.get(queryURL)
+.then((data) => {
+
+
+});
