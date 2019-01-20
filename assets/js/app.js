2a42d3f3ddcb07c469180e0f356218d02fd4ce15
johnale <johnale@cisco.com>
2019-01-19 20:22:46 -0500

    added results clear on button click, added button instead of submit for clear button, added validation to form input

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,76 @@
 
 
-
-$.get('https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20150101&end_date=20160101&q=war&sort=newest&api-key=DUboS1XV1GSEARcYle8Upu5f9MORBuaq')
-.then((data) => {
-    const articleData = data.response.docs;
-    let count = 5;
-
-    for (let i = 1; i < count; i++) {
-        var div = $('<div class="article-space"></div>');
-        const contentDiv = $('<div class="article-content">');
-        var titleHeader = $('<p class="rowheader">');
-        var author = $('<p class="author" style="font-weight: bolder;">');
-        var p1 = $('<p>');
-        var p2 = $('<p>');
-        var p3 = $('<p>');
-
+const getApiData = (api, limitValue) => {
+    $.get(api)
+    .then((data) => {
+        const articleData = data.response.docs;
+        $('.article-space').remove();
+        
     
+        for (let i = 0; i < limitValue; i++) {
+            var div = $('<div class="article-space"></div>');
+            const contentDiv = $('<div class="article-content">');
+            var titleHeader = $('<p class="rowheader">');
+            var author = $('<p class="author" style="font-weight: bolder;">');
+            var p1 = $('<p>');
+            var p2 = $('<p>');
+            var p3 = $('<p>');
+    
+        
+    
+            titleHeader.html(articleData[i].headline.main);
+            p1.html(articleData[i].pub_date);
+            p2.html(articleData[i].snippet);
+            author.html(articleData[i].byline.original);
+            p3.html(articleData[i].web_url);
+    
+            contentDiv.append( p1, p2, p3, author);
+            div.append(titleHeader, contentDiv);
+            $("#article-container").append(div);
+        }
+    
+    })
+    .catch((error) => console.log(`you have an error: ${error}`));
+}
 
-        titleHeader.html(articleData[i].headline.main);
-        p1.html(articleData[i].pub_date);
-        p2.html(articleData[i].snippet);
-        author.html(articleData[i].byline.original);
-        p3.html(articleData[i].web_url);
 
-        contentDiv.append( p1, p2, p3, author);
-        div.append(titleHeader, contentDiv);
-        $("#article-container").append(div);
-    }
+    // $('.clear-reset').prop('disabled', true);
+    $('.search-form').on('submit', function(event) {
+        event.preventDefault();
+        
+        let searchEvent;
+        let startDate;
+        let endDate;
+        if (event.currentTarget[0].value) {
+            searchEvent = (!event.currentTarget[2].value || !event.currentTarget[3].value ) ? `q=${event.currentTarget[0].value}` : `&q=${event.currentTarget[0].value}`;
+        } else return false;
+        
+        const limit = event.currentTarget[1].value;
+        if (event.currentTarget[2].value.length < 8) {
+            startDate = event.currentTarget[2].value + '0101';
+        } else {
+            startDate = event.currentTarget[2].value ? `begin_date=${event.currentTarget[2].value}` : '';
+        }
+        console.log(startDate);
 
-})
-.catch((error) => console.log(`you have an error: ${error}`));
+        if (event.currentTarget[2].value.length < 8) {
+            endDate = event.currentTarget[3].value + '1231';
+
+        } else {
+            endDate = event.currentTarget[3].value ? `&end_date=${event.currentTarget[3].value}` : '';
+
+        }
+        console.log(endDate);
+
+        const baseUrl = 'https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?'
+
+        searchEvent = searchEvent.split(" ").join("+");
+        
+        const url = `${baseUrl}${startDate}${endDate}${searchEvent}&sort=newest&api-key=DUboS1XV1GSEARcYle8Upu5f9MORBuaq`;
+
+        
+
+        getApiData(url, limit);
+    });
+
+    $('.clear-reset').on('click', () => $('.article-space').remove());
